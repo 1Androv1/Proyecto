0574e7cfc62340b8a48de5943454768fbeee59e4
deividcool <velozadeivid@gmail.com>
2024-04-21 19:29:20 -0500

    update carpeta front

--- FrontTodoAppVuez/src/App.jsx
+++ FrontTodoAppVuez/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <main className='flex flex-1 h-screen w-full bg-[#0E0F12] flex-col xl:w-[1280px]'>
       <Header />
-      <div className='flex flex-row flex-1 p-4 justify-evenly'>
+      <div className='flex flex-row flex-1 p-4 justify-between'>
         <PrincipalCard Tittle={"TO-DO"} />
         <PrincipalCard Tittle={"IN-PROOGRESS"} />
         <PrincipalCard Tittle={"COMPLETED"} />
