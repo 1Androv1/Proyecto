e715511fde76e65cc5480256aa5d4c3ede28542f
Alejandro Rojas Martinez <androv389@gmail.com>
2024-04-24 04:08:50 -0500

    update change status funcionalidad

--- FrontTodoAppVuez/src/coneccions/api.jsx
+++ FrontTodoAppVuez/src/coneccions/api.jsx
@@ -1,3 +1,4 @@
 export const ApiLogin = `http://localhost:5235/api/users/loginUser`;
 export const GetAllTask = `http://localhost:5235/api/allTask`;
 export const ApiRegister = `http://localhost:5235/api/users`;
+export const changeStatus = `http://localhost:5235/api/changeStatus`;
